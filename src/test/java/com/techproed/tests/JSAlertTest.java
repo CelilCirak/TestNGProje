90852668d2e3a6edda0a2129519493e3acee9394
CelilCirak <celilcirak@gmail.com>
2020-09-03 16:20:25 -0400

    Day12 Ders sonu

---
+++ src/test/java/com/techproed/tests/JSAlertTest.java
@@ -0,0 +1,93 @@
+package com.techproed.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+  public class JSAlertTest {
+
+      private WebDriver driver;
+
+      @BeforeClass
+      public void setUp() {
+          WebDriverManager.chromedriver().setup();
+          driver = new ChromeDriver();
+          driver.manage().window().maximize();
+          driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+      }
+
+      @Test()
+      public void jsAlertTest() {
+          driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+          // <button onclick="jsAlert()">Click for JS Alert</button>
+          // xpath, cssSelector, tagName,
+          WebElement button = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+          button.click();
+
+          // Alert'in icerdigi mesaji almalk icin .getText() kullanitiz.
+          String alertMesaji = driver.switchTo().alert().getText();
+          System.out.println(alertMesaji);
+
+          //alert butonuna tiklamak icin.
+          driver.switchTo().alert().accept();
+
+      }
+
+      @Test()
+      public void jsAConfirmTest() {
+          driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+          WebElement button = driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+          button.click();
+
+          // Alert'in icerdigi mesaji almalk icin .getText() kullanitiz.
+          String alertMesaji = driver.switchTo().alert().getText();
+          System.out.println(alertMesaji);
+
+          //alert butonuna tiklamak icin.
+          driver.switchTo().alert().accept();
+
+          String mesaj = driver.switchTo().alert().getText();
+          System.out.println(mesaj);
+
+          // alert icindeki cancel butonuna tiklar.
+          driver.switchTo().alert().dismiss();
+
+      }
+
+      @Test()
+      public void jsAPromptTest() {
+          driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+          WebElement button = driver.findElement(By.xpath("//button[@onclick='jsPrompt()']"));
+          button.click();
+
+          // Alert'in icerdigi mesaji almalk icin .getText() kullanitiz.
+          String mesaj = driver.switchTo().alert().getText();
+          System.out.println(mesaj);
+
+          //alert prompt a mesaj gondermek icin.
+          driver.switchTo().alert().sendKeys("Merhaba TestNG");
+
+          try {
+              Thread.sleep(5000);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+          driver.switchTo().alert().accept();
+
+          //  String mesaj = driver.switchTo().alert().getText();
+          //  System.out.println(mesaj);
+
+          // alert icindeki cancel butonuna tiklar.
+          // driver.switchTo().alert().dismiss();
+
+      }
+
+  }
---
+++ src/test/java/com/techproed/tests/SoftAssertTest.java
@@ -0,0 +1,107 @@
+package com.techproed.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+public class SoftAssertTest {
+
+    /*Assert.assertTrue
+      Assert.assertFalse
+      Assert.assertEquals
+
+      SoftAssert kullaniminin HardAssert kullanimindan farklari:
+      1. SofAssert'te eger test basarisiz olursa, kalan kisim calistirilir.
+      2. SoftAssert clasindan obje olusturmak zorundayiz.
+
+      Assert=>
+      Verify=>
+
+
+
+     */
+
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+    }
+
+    @Test
+    public void test01(){
+        driver.get("http://amazon.com");
+
+        String baslik = driver.getTitle();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertFalse(baslik.contains("Amazon"));
+        softAssert.assertTrue(baslik.contains("Car"));
+        softAssert.assertEquals("Online", baslik);
+        softAssert.assertAll(); // yukarıdaki yaptığımız tüm doğrulamalar
+        // başarılı ise testimiz başarılı, değilse
+        // testimiz başarısız.
+        // HardAssert gibi
+
+
+    }
+
+    @Test
+    public void test02(){
+        driver.get("http://a.testaddressbook.com/sign_in");
+
+        SoftAssert softAssert = new SoftAssert();
+
+        WebElement email = driver.findElement(By.id("session_email"));
+        email.sendKeys("testtechproed@gmail.com");
+        softAssert.assertTrue(email.getText().equals("testtechproed@gmail.com"));
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        softAssert.assertTrue(driver.getTitle().equals("Deneme"));
+
+
+        WebElement sifreKutusu = driver.findElement(By.id("session_password"));
+        sifreKutusu.sendKeys("Test1234!");
+        softAssert.assertTrue(sifreKutusu.getText().equals("testtechproed@gmail.com"));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        softAssert.assertTrue(driver.getTitle().equals("Address book"));
+
+        WebElement signInLinki = driver.findElement(By.name("commit"));
+        signInLinki.click();
+
+        WebElement signOutLinki = driver.findElement(By.partialLinkText("Sign Out"));
+        boolean varMi = signOutLinki.isDisplayed();
+
+       softAssert.assertAll();
+    }
+
+
+    @AfterClass
+    public void tearDown(){
+       // driver.quit();
+
+    }
+}
+
